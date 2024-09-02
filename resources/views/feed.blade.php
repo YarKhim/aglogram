1807ae6ee7c0bb5465e9c2886f6e621da902e4a4
YarKhim <160514711+YarKhim@users.noreply.github.com>
2024-09-02 23:06:26 +0300

    YarKhim

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -7,36 +7,126 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Главная страница") }}
-                    <div class="post">
-                        <div class="avatar">
-                        </div>
-                        <div class="username">
-                            <h2>Никола Тесла</h2>
-                        </div>
-                        <div class="timecode">
-                            <sup>18:34</sup>
-                        </div>
-                        <div class="foto">
-                            <p>фото<br>фото<br>фото<br>фото<br>фото<br>фото<br>фото<br>фото<br>фото<br>фото<br>фото<br>фото<br>фото<br>r>фото<br>фотофото<br>фото<br>фото<</p>
-                        </div>
-                        <div class="description">
-                            <p>описание</p>
-                        </div>
-                        <div class="likes" >
-                            <img src="heart.png" width="30px">
-                        </div>
-                        <div class="comments" >
-                            <img src="heart.png" width="30px">
-                        </div>
-                        <div class="share" >
-                            <img src="heart.png" width="30px">
-                        </div>
+            {{-- <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"> --}}
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                {{-- {{ __("Главная страница") }} --}}
+                <div class="post border_debug">
+                    <div class="avatar">
+                        <img src="http://localhost:8000/avatar/7">
+
                     </div>
-                </div>                    
+                    <div class="username">
+                        <h2>Ярослав Хаймусов</h2>
+                    </div>
+
+                    <div class="timecode">
+                        <sup>18:34</sup>
+                    </div>
+                    <div class="foto">
+                        <img src="http://localhost:8000/avatar/7">
+
+                    </div>
+                    <div class="description">
+                        <p>Хаймусов Ярослав сменил фото профиля</p>
+                    </div>
+                    <div class="likes">
+                        <img src="heart.png" width="30px">
+                    </div>
+                    <div class="comments">
+                        <img src="heart.png" width="30px">
+                    </div>
+                    <div class="share">
+                        <img src="heart.png" width="30px">
+                    </div>
+                </div>
+                <div class="post">
+                    <div class="avatar">
+                        <img src="http://localhost:8000/avatar/7">
+
+                    </div>
+                    <div class="username">
+                        <h2>Ярослав Хаймусов</h2>
+                    </div>
+                    <div class="timecode">
+                        <sup>18:34</sup>
+                    </div>
+                    <div class="foto">
+                        <img src="http://localhost:8000/avatar/7">
+
+                    </div>
+                    <div class="description">
+                        <p>а тут у НАС дебаг цуймашйй йййййгрц уарцйу шгарцйу гшрай шцураг йцуппа йп амгйу карпу гшшшшш шшшшшшш шшшшшш шшшш шшшш шш шшшшшш шшшшшш шшшшш шшш шшшшшш шшш</p>
+                    </div>
+                    <div class="likes">
+                        <img src="heart.png" width="30px">
+                    </div>
+                    <div class="comments">
+                        <img src="heart.png" width="30px">
+                    </div>
+                    <div class="share">
+                        <img src="heart.png" width="30px">
+                    </div>
+                </div>
+                <div class="post">
+                    <div class="avatar">
+                        <img src="http://localhost:8000/avatar/7">
+                    </div>
+                    <div class="username">
+                        <h2>Ярослав Хаймусов</h2>
+                    </div>
+                    <div class="timecode">
+                        <sup>18:34</sup>
+                    </div>
+                    <div class="foto">
+                        <img src="http://localhost:8000/avatar/7">
+
+                    </div>
+                    <div class="description">
+                        <p>описание</p>
+                    </div>
+                    <div class="likes">
+                        <img src="heart.png" width="30px">
+                    </div>
+                    <div class="comments">
+                        <img src="heart.png" width="30px">
+                    </div>
+                    <div class="share">
+                        <img src="heart.png" width="30px">
+                    </div>
+                </div>
+                <div class="post">
+                    <div class="avatar">
+                        <img src="http://localhost:8000/avatar/7">
+
+                    </div>
+                    <div class="username">
+                        <h2>Ярослав Хаймусов</h2>
+                    </div>
+
+                    <div class="timecode">
+                        <sup>18:34</sup>
+                    </div>
+                    <div class="foto">
+                        <img src="http://localhost:8000/avatar/7">
+
+                    </div>
+                    <div class="description">
+                        <p>описание</p>
+                    </div>
+                    <div class="likes">
+                        <img src="heart.png" width="30px">
+                    </div>
+                    <div class="comments">
+                        <img src="heart.png" width="30px">
+                    </div>
+                    <div class="share">
+                        <img src="heart.png" width="30px">
+                    </div>
+                </div>
+
+
             </div>
+            {{-- </div> --}}
         </div>
     </div>
 
